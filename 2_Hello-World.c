7241e89e15d7581eb5f74ca5bc823b1302b6637f
SrG <SG345@keemail.me>
2017-05-19 18:10:56 -0500

    Create 2_Hello-World.c

---
+++ 2_Hello-World.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main( int argc, char *argv[] )
+{
+
+	puts("Hello World."); //Puts a new line in already
+	
+	printf(">:)\n" );
+	
+	return 0;
+	
+}
