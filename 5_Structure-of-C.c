658677d581d7be2d7ff4cf631681e79af7afe1b4
SrG <SG345@keemail.me>
2017-05-19 18:22:41 -0500

    Create 5_Structure-of-C.c

---
+++ 5_Structure-of-C.c
@@ -0,0 +1,21 @@
+/*
+An "include" is the way to import the contents of one
+ﬁle into this source ﬁle. 
+C has a convention of using .h extensions for "header"
+ﬁles, which then contain lists of functions you want to use in your program
+*/
+
+#include <stdio.h>
+
+/* this is a comment */
+int main( int argc, char *argv[] )
+{
+
+	//this is also a comment
+	int distance = 100;
+
+	printf("You are %d miles away.\n", distance);
+
+
+	return 0;
+}
